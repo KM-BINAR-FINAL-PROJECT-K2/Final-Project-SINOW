227d38b941f7ac6dfd765f1fee617208aea90e64
Adella Desinta <adelladesinta88@gmail.com>
2023-11-22 19:07:01 +0800

    menambahkan API create,read, dan delete module

--- BE/routes/index.js
+++ BE/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const Course = require("./courseRoutes");
+const Module = require("./moduleRoutes");
 
 router.use("/api/v1/courses", Course);
+router.use("/api/v1/modules", Module);
 
 module.exports = router;
