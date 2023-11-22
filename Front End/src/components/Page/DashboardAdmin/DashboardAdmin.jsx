a5beba2cd7f324f8b02a58e25d5b0c7c7697a8bc
unknown <aliframdani279@gmail.com>
2023-11-22 18:23:59 +0700

    Menambahkan Logo Product dan Membuat Responsive Table Dashboard

--- Front End/src/components/Page/DashboardAdmin/DashboardAdmin.jsx
+++ Front End/src/components/Page/DashboardAdmin/DashboardAdmin.jsx
@@ -1,6 +1,6 @@
 import Navigation from "../../Template/Navigation/Navigation";
 import Card from "../../Molecule/Card/Card";
-import TableBody from "../../Molecule/TablePayment/TablePayment";
+import PaymentTable from "../../Molecule/PaymentTable/PaymentTable";
 
 export default function DashboadAdmin() {
   return (
@@ -23,40 +23,42 @@ export default function DashboadAdmin() {
         />
       </section>
 
-      <section className="mx-16">
-        <div className="py-[10px] flex">
-          <h2 className="my-[10px] font-semibold text-[20px] flex-1">
+      <section className="mx-8 lg:mx-16">
+        <div className="py-[10px] flex flex-wrap">
+          <h2 className="my-[10px] font-semibold text-[20px] flex-wrap flex-1 min-w-[200px]">
             Status Pembayaran
           </h2>
-
-          <button className="bg-neutral-01 border-2 border-darkblue-05 inline-block rounded-[16px] py-[5px] px-[10px] w-[125px] h-[34px] mr-[16px] my-[10px]">
-            <div className="flex gap-[8px] items-center justify-center">
+          <div className="flex">
+            <button className="bg-neutral-01 border-2 border-darkblue-05 inline-block rounded-[16px] py-[5px] px-[10px] w-[125px] h-[34px] mr-[16px] my-[10px]">
+              <div className="flex gap-[8px] items-center justify-center">
+                <img
+                  src="/images/prefix-wrapper.png"
+                  alt=""
+                  className="w-[24px] h-[24px]"
+                />
+                <span className="text-[16px] font-semibold text-darkblue-05">
+                  Filter
+                </span>
+              </div>
+            </button>
+            <button className="">
               <img
-                src="/images/prefix-wrapper.png"
+                src="/images/search-icon-2.png"
                 alt=""
-                className="w-[24px] h-[24px]"
+                className=" w-[24px] h-[24px] inline-block"
               />
-              <span className="text-[16px] font-semibold text-darkblue-05">
-                Filter
-              </span>
-            </div>
-          </button>
-          <button className="my-[10px]">
-            <img
-              src="/images/search-icon-2.png"
-              alt=""
-              className="w-[24px] h-[24px] inline-block"
-            />
-          </button>
+            </button>
+          </div>
         </div>
       </section>
 
-      <section className="w-full px-[63px]">
-        <div>
-          <section className="w-full">
+      <section className="border-red-500 mx-8 n lg:mx-[64px] mb-64 ">
+        <div className="border-blue-500 ">
+          <section className="border-yellow-300 overflow-auto">
+            {/* Payment Table */}
             <table className="w-full table-auto">
-              <thead>
-                <tr className="bg-lightblue-05 text-left">
+              <thead className="sticky top-0 bg-lightblue-05 z-10">
+                <tr className="bg-lightblue-05 text-left border-orange-700">
                   <th className="py-2 text-[12px] px-4 font-semibold w-1/7">
                     ID
                   </th>
@@ -78,7 +80,7 @@ export default function DashboadAdmin() {
                 </tr>
               </thead>
               <tbody>
-                <TableBody
+                <PaymentTable
                   color={"bg-alert-success"}
                   id={"johndoe123"}
                   category={"UI/UX Design"}
@@ -87,7 +89,7 @@ export default function DashboadAdmin() {
                   paymenMethod={"Credit Card"}
                   paymentDate={"21 Sep, 2023 at 2:00 AM"}
                 />
-                <TableBody
+                <PaymentTable
                   color={"bg-alert-danger"}
                   id={"supermanxx"}
                   category={"UI/UX Design"}
