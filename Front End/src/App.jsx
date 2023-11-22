92d9a63fecda00706926763babe4a5c4e43db142
accenggg <arseliusngiklan@gmail.com>
2023-11-22 23:30:06 +0800

    refaktor file, folder, dan route

--- Front End/src/App.jsx
+++ Front End/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CRUD from "./components/Page/CRUD/CRUD";
 import DashboadAdmin from "./components/Page/DashboardAdmin/DashboardAdmin";
+import Login from "./components/Login/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashboadAdmin location={"/"} />} />
+        <Route
+          path="/dashboard"
+          element={<DashboadAdmin location={"/dashboard"} />}
+        />
         <Route
           path="/kelola-kelas"
           element={<CRUD location={"/kelola-kelas"} />}
         />
-        <Route path="/login" element={<Login location={"/login"} />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
