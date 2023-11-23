6a20ef9de13ac0e22fbe796004c76bc7b7c4e994
accenggg <arseliusngiklan@gmail.com>
2023-11-23 22:58:35 +0800

    membuat sidebar responsive design - final

--- Front End/src/components/Template/Navigation/Navigation.jsx
+++ Front End/src/components/Template/Navigation/Navigation.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable react/prop-types */
 import Sidebar from "../../Organism/Sidebar/Sidebar";
 import Header from "../../Organism/Header/Header";
+import SmallSidebar from "../../Organism/Sidebar/SmallSidebar";
 export default function Navigation({ children }) {
   return (
-    <main className="flex">
-      <Sidebar />
-      <section className="w-full">
-        <Header name={"Admin"} />
-        {children}
-      </section>
-    </main>
+    <>
+      <SmallSidebar />
+      <main className="flex flex-auto">
+        <Sidebar />
+        <section className="w-full">
+          <Header name={"Admin"} />
+          {children}
+        </section>
+      </main>
+    </>
   );
 }
