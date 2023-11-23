25a04954319cd75d6248f2bbeb0b376fc67c511a
Adella Desinta <adelladesinta88@gmail.com>
2023-11-23 23:17:09 +0800

    menambahkan API CRUD untuk chapter

---
+++ BE/controllers/chapterController.js
@@ -0,0 +1,181 @@
+const { Module, Chapter, Course } = require("../models");
+const ApiError = require("../utils/ApiError");
+
+const createChapter = async (req, res, next) => {
+  try {
+    let { no, name, courseId } = req.body;
+
+    if (!no || !name || !courseId) {
+      return next(new ApiError("Semua field harus di isi", 400));
+    }
+
+    const checkCourse = await Course.findByPk(courseId);
+    if (!checkCourse) {
+      return next(new ApiError("Course tidak ditemukan", 404));
+    }
+
+    const chapter = await Chapter.create({
+      no,
+      name,
+      courseId,
+    });
+
+    if (!chapter) {
+      return next(new ApiError("Gagal membuat chapter", 500));
+    }
+
+    res.status(201).json({
+      status: "succes",
+      message: "Sukses membuat chapter",
+      data: chapter,
+    });
+  } catch (error) {
+    return next(new ApiError(error.message, 500));
+  }
+};
+
+const getAllChapter = async (req, res, next) => {
+  try {
+    const chapters = await Chapter.findAll({
+      include: [
+        {
+          model: Module,
+          as: "modules",
+        },
+      ],
+      order: [["id", "ASC"]],
+    });
+
+    if (!chapters || chapters.length === 0) {
+      return next(new ApiError("Chapter tidak ditemukan", 404));
+    }
+
+    res.status(200).json({
+      status: "succes",
+      message: "Berhasil mendapatkan data chapter",
+      data: chapters,
+    });
+  } catch (error) {
+    return next(new ApiError(error.message, 500));
+  }
+};
+const getChapterById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const chapter = await Chapter.findByPk(id, {
+      include: [
+        {
+          model: Module,
+          as: "modules",
+        },
+      ],
+    });
+
+    if (!chapter || chapter.length === 0) {
+      return next(new ApiError("Chapter tidak ditemukan", 404));
+    }
+
+    res.status(200).json({
+      status: "succes",
+      message: `Berhasil mendapatkan data Chapter id: ${id};`,
+      data: chapter,
+    });
+  } catch (error) {
+    return next(new ApiError(error.message, 500));
+  }
+};
+
+const updateChapter = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    let { no, name, courseId } = req.body;
+
+    const chapter = await Chapter.findByPk(id);
+    if (!chapter) {
+      return next(new ApiError("Chapter tidak ditemukan", 404));
+    }
+
+    const updateData = {};
+
+    if (no) {
+      updateData.no = no;
+    }
+
+    if (name) {
+      updateData.name = name;
+    }
+
+    if (courseId) {
+      const course = await Course.findByPk(courseId);
+      if (!course) {
+        return next(new ApiError("Course tidak ditemukan", 404));
+      }
+      updateData.courseId = courseId;
+    }
+
+    const updatedChapter = await Chapter.update(updateData, {
+      where: {
+        id,
+      },
+      returning: true,
+    });
+
+    if (!updatedChapter) {
+      return next(new ApiError("Gagal update Chapter", 500));
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: `Berhasil mengupdate data chapter id: ${id}`,
+      data: updatedChapter,
+    });
+  } catch (error) {
+    next(new ApiError(error, 500));
+  }
+};
+const deleteChapter = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const chapter = await Chapter.findByPk(id, {
+      include: [
+        {
+          model: Module,
+          as: "modules",
+        },
+      ],
+    });
+
+    if (!chapter) {
+      return next(new ApiError("Chapter tidak ditemukan", 404));
+    }
+
+    if (chapter.modules && chapter.modules.length > 0) {
+      return next(new ApiError("Tidak dapat menghapus chapter dengan modul terkait", 400));
+    }
+
+    const isChapterDeleted = await Chapter.destroy({
+      where: {
+        id,
+      },
+    });
+    if (!isChapterDeleted) {
+      return next(new ApiError("Gagal menghapus Chapter", 500));
+    }
+
+    res.status(200).json({
+      status: "succes",
+      message: `Berhasil menghapus data Chapter id: ${id};`,
+      data: Chapter,
+    });
+  } catch (error) {
+    return next(new ApiError(error.message, 500));
+  }
+};
+
+module.exports = {
+  createChapter,
+  getAllChapter,
+  getChapterById,
+  deleteChapter,
+  updateChapter,
+};
---
+++ BE/routes/chapterRouter.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const Chapter = require("../controllers/chapterController");
+const authenticate = require("../middlewares/authenticate");
+const checkRole = require("../middlewares/checkRole");
+
+router.post("/", authenticate, checkRole("admin"), Chapter.createChapter);
+router.get("/", Chapter.getAllChapter);
+router.get("/:id", Chapter.getChapterById);
+router.put("/:id", authenticate, checkRole("admin"), Chapter.updateChapter);
+router.delete("/:id", authenticate, checkRole("admin"), Chapter.deleteChapter);
+
+module.exports = router;
