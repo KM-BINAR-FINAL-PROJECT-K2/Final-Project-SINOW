ad2712c0282f8c9875c921e369c0c59f6223aaf7
unknown <aliframdani279@gmail.com>
2023-11-29 21:30:48 +0700

    Merapihkan tampilan Logout

--- Front End/src/components/Page/Logout/Logout.jsx
+++ Front End/src/components/Page/Logout/Logout.jsx
@@ -13,7 +13,7 @@ export default function Logout({ toggleShowLogout }) {
       <div className="fixed z-[1000] bg-black opacity-40 top-0 left-0 right-0 bottom-0"></div>
       <div className="absolute z-[1000] top-0 left-0 right-0 bottom-0 ">
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <div className="border rounded-[16px] bg-neutral-01 w-[350px] sm:w-[500px] md:w-[600px] lg:w-[750px] h-auto py-[10px] text-center">
+          <div className="border rounded-[16px] bg-neutral-01 w-[300px] sm:w-[400px]  h-auto py-[10px] text-center">
             <div className="justify-center items-center">
               <h3 className="text-darkblue-05 font-semibold text-[20px] m-[20px]">
                 Keluar Dari Akun?
