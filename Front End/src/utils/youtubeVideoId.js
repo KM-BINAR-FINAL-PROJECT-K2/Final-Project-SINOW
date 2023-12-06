96a3f0803a8021981b3019fec03ee3f6ef32fe11
accenggg <arseliusngiklan@gmail.com>
2023-12-07 00:44:23 +0800

    styling info kelas (unfinished)

---
+++ Front End/src/utils/youtubeVideoId.js
@@ -0,0 +1,7 @@
+export default function youtubeVideoId(url) {
+  const regex =
+    /(?:youtu\.be\/|youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
+  const match = url.match(regex);
+
+  return match ? match[1] : null;
+}
