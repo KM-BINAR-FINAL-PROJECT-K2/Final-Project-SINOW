a70d16a94aa08ef7a1030673cd0d32a3fc94ca02
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2023-12-10 07:51:09 +0700

    memperbaiki bug di error handler

--- BE/controllers/errorController.js
+++ BE/controllers/errorController.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
-  const error = { ...err }
-  error.statusCode = error.statusCode || 500
+  const error = {}
+  error.statusCode = err.statusCode || 500
+  error.status = err.status || 'Error'
+  error.message = err.message || 'Internal Server Error'
 
   return res.status(error.statusCode).json({
     status: error.status,
