ed400cd6ccb91784d7e32f518ed9e905176660ec
accenggg <arseliusngiklan@gmail.com>
2023-12-21 20:11:43 +0800

    setup get category

--- Front End/src/components/Molecule/ClassTable/ClassTable.jsx
+++ Front End/src/components/Molecule/ClassTable/ClassTable.jsx
@@ -1,28 +1,146 @@
 /* eslint-disable react/prop-types */
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { rupiah } from "../../../utils/formatCurrency";
+import { Flowbite, Dropdown } from "flowbite-react";
 import Loading from "../Loading/Loading";
 import { LoaderContext } from "../../../store/Loader";
 import { ClassContext } from "../../../store/ClassStore";
 import { InfoClassContext } from "../../../store/InfoClassUI";
 import { RemoveClassContext } from "../../../store/RemoveClassUI";
 import { ErrorContext } from "../../../store/Error";
+import { FaPlus } from "react-icons/fa6";
 export default function ClassTable() {
-  const { isLoading } = useContext(LoaderContext);
-  const { isError } = useContext(ErrorContext);
+  const { isLoading, setIsLoading } = useContext(LoaderContext);
+  const { isError, setIsError } = useContext(ErrorContext);
   const { classSinow } = useContext(ClassContext);
   const { toggleShowInfo } = useContext(InfoClassContext);
   const { toggleShowWarning } = useContext(RemoveClassContext);
+  const [categories, setCategories] = useState();
+
+  const customTheme = {
+    dropdown: {
+      arrowIcon: "ml-2 h-4 w-4 text-black",
+      content: "p-1 focus:outline-none",
+      floating: {
+        animation: "transition-opacity",
+        arrow: {
+          base: "absolute z-10 h-2 w-2 rotate-45",
+          style: {
+            dark: "bg-black border border-black fill-black ",
+            light: "bg-black border border-black fill-black",
+            auto: "bg-black border border-black fill-black ",
+          },
+          placement: "-4px",
+        },
+        base: "z-10 w-fit rounded divide-y divide-gray-100 shadow focus:outline-none",
+        content: "py-1 text-sm text-white",
+        divider: "my-1 h-px bg-gray-100 ",
+        header: "block py-2 px-4 text-sm text-gray-700",
+        hidden: "invisible opacity-0",
+        item: {
+          container: "",
+          base: "flex items-center justify-start py-2 px-4 text-sm text-white cursor-default w-full bg-sinow-05 hover:bg-white hover:text-sinow-05 hover:stroke-sinow-05 focus:bg-gray-100 focus:outline-none",
+          icon: "mr-2 h-4 w-4",
+        },
+        style: {
+          dark: "bg-gray-900 text-sinow-05",
+          light: "bg-sinow-05 text-gray-900",
+          auto: "bg-sinow-05 text-gray-900 ",
+        },
+        target: "w-fit",
+      },
+      inlineWrapper: "flex items-center",
+    },
+  };
+
+  useEffect(() => {
+    try {
+      const getCategories = async () => {
+        setIsLoading(true);
+        const response = await axios.get(
+          "http://localhost:3000/api/v1/category"
+        );
+        setCategories(response.data.data);
+      };
+      getCategories();
+    } catch (error) {
+      setIsError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
   return (
     <table className="w-full snap-mandatory snap-x table-auto">
       <thead className="sticky top-0 bg-lightblue-05 z-10">
-        <tr className="bg-lightblue-05 text-left">
-          <th className="py-2 text-[12px] px-4 font-semibold w-1/7">
+        <tr className="bg-lightblue-05 text-left text-gray-800">
+          <th className="py-2 text-[12px] px-4 font-semibold w-1/7 ">
             Kode Kelas
           </th>
-          <th className="px-4 py-2 text-[12px] font-semibold w-1/7">
-            Kategori
+          <th className=" py-2 text-[12px] font-semibold w-1/7">
+            {/* <div className="flex items-center gap-3">
+              <span>Kategori</span>
+              <button className="cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="rgb(0 204 244)"
+                  // eslint-disable-next-line react/no-unknown-property
+                  dataSlot="icon"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div> */}
+            <Flowbite theme={{ theme: customTheme }}>
+              <Dropdown
+                label="Kategori"
+                placement="right-start"
+                style={{
+                  color: "black",
+                  border: "none",
+                  textAlign: "left",
+                  fontWeight: "600",
+                }}
+              >
+                {categories && (
+                  <>
+                    {categories.map((category) => (
+                      <Dropdown.Item key={category.id}>
+                        <div className="flex items-center gap-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            className="w-6 h-6 stroke-gray-700 cursor-pointer hover:stroke-2"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                            />
+                          </svg>
+                          <input
+                            className="bg-transparent"
+                            type="text"
+                            value={category.name}
+                          />
+                        </div>
+                      </Dropdown.Item>
+                    ))}
+                    <Dropdown.Divider />
+                    <Dropdown.Item>Tambah Kategori</Dropdown.Item>
+                  </>
+                )}
+              </Dropdown>
+            </Flowbite>
           </th>
           <th className="px-4 py-2 text-[12px] font-semibold w-[150px] lg:w-2/7">
             Nama Kelas
