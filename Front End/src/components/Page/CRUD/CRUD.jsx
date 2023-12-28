dc4911f042f6c8d7a3184ece87c4269a59d8b225
accenggg <arseliusngiklan@gmail.com>
2023-12-29 00:58:57 +0800

    remove tooltip

--- Front End/src/components/Page/CRUD/CRUD.jsx
+++ Front End/src/components/Page/CRUD/CRUD.jsx
@@ -25,7 +25,6 @@ import { FilterClassContext } from "../../../store/FilterClass";
 import { SearchValueContext } from "../../../store/SearchValue";
 import { CategoryContainerContext } from "../../../store/CategoryUI";
 import { RandomNumberContext } from "../../../store/RandomNumber";
-import { Tooltip } from "flowbite-react";
 
 export default function CRUD() {
   const { setIsLoading } = useContext(LoaderContext);
@@ -137,31 +136,29 @@ export default function CRUD() {
             <h2 className="my-[10px] font-semibold text-[20px] flex-wrap flex-1 min-w-[200px]">
               Kelola Kelas
             </h2>
-            <Tooltip content="Kelola Notifikasi" placement="bottom">
-              <a
-                className={`flex items-center border-sinow-05 rounded-md border-2 mr-[18px] my-[10px] gap-[7px] py-[3px] px-[10px] hover:cursor-pointer ${
-                  !showSearchInput
-                    ? "fill-sinow-05 text-sinow-05 hover:fill-white hover:text-white hover:bg-sinow-05"
-                    : "fill-white text-white hover:fill-sinow-05 hover:text-sinow-05 bg-sinow-05 hover:bg-white"
-                }`}
-                href="/kelola-notifikasi"
+            <a
+              className={`flex items-center border-sinow-05 rounded-md border-2 mr-[18px] my-[10px] gap-[7px] py-[3px] px-[10px] hover:cursor-pointer ${
+                !showSearchInput
+                  ? "fill-sinow-05 text-sinow-05 hover:fill-white hover:text-white hover:bg-sinow-05"
+                  : "fill-white text-white hover:fill-sinow-05 hover:text-sinow-05 bg-sinow-05 hover:bg-white"
+              }`}
+              href="/kelola-notifikasi"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="w-6 h-6"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="w-6 h-6"
-                >
-                  <path d="M5.85 3.5a.75.75 0 0 0-1.117-1 9.719 9.719 0 0 0-2.348 4.876.75.75 0 0 0 1.479.248A8.219 8.219 0 0 1 5.85 3.5ZM19.267 2.5a.75.75 0 1 0-1.118 1 8.22 8.22 0 0 1 1.987 4.124.75.75 0 0 0 1.48-.248A9.72 9.72 0 0 0 19.266 2.5Z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25A6.75 6.75 0 0 0 5.25 9v.75a8.217 8.217 0 0 1-2.119 5.52.75.75 0 0 0 .298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 1 0 7.48 0 24.583 24.583 0 0 0 4.83-1.244.75.75 0 0 0 .298-1.205 8.217 8.217 0 0 1-2.118-5.52V9A6.75 6.75 0 0 0 12 2.25ZM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 0 0 4.496 0l.002.1a2.25 2.25 0 1 1-4.5 0Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <path d="M5.85 3.5a.75.75 0 0 0-1.117-1 9.719 9.719 0 0 0-2.348 4.876.75.75 0 0 0 1.479.248A8.219 8.219 0 0 1 5.85 3.5ZM19.267 2.5a.75.75 0 1 0-1.118 1 8.22 8.22 0 0 1 1.987 4.124.75.75 0 0 0 1.48-.248A9.72 9.72 0 0 0 19.266 2.5Z" />
+                <path
+                  fillRule="evenodd"
+                  d="M12 2.25A6.75 6.75 0 0 0 5.25 9v.75a8.217 8.217 0 0 1-2.119 5.52.75.75 0 0 0 .298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 1 0 7.48 0 24.583 24.583 0 0 0 4.83-1.244.75.75 0 0 0 .298-1.205 8.217 8.217 0 0 1-2.118-5.52V9A6.75 6.75 0 0 0 12 2.25ZM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 0 0 4.496 0l.002.1a2.25 2.25 0 1 1-4.5 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
 
-                <span className="text-sm font-semibold">Kelola Notifikasi</span>
-              </a>
-            </Tooltip>
+              <span className="text-sm font-semibold">Kelola Notifikasi</span>
+            </a>
             <div className="flex items-center z-50 ">
               {!showSearchInput && (
                 <a
